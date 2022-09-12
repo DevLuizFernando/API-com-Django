1eb9885136ec12d099ffe9a25912b3f21b69c9d4
Luiz Fernando <luizfernando80500@gmail.com>
2022-09-12 12:28:05 -0300

    first commit

---
+++ escola/test/tests_cursos.py
@@ -0,0 +1,45 @@
+from rest_framework.test import APITestCase
+from escola.models import Curso
+from django.urls import reverse
+from rest_framework import status
+
+class CursosTestCase(APITestCase):
+
+    def setUp(self):
+        self.list_url = reverse('Cursos-list')
+        self.curso_1 = Curso.objects.create(
+            codigo_curso='CT1', descricao='Curso teste 1', nivel='I'
+        )
+        self.curso_2 = Curso.objects.create(
+            codigo_curso='CT2', descricao='Curso teste 2', nivel='A'
+        )
+
+    def test_requisicao_get_para_listar_cursos(self):
+        """Teste para verificar a requisicao get para listar os cursos"""
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_requisicao_post_para_criar_curso(self):
+        """Teste para verificar a requisicao POST para criar cursos"""
+        data = {
+            'codigo_curso':'CT2', 
+            'descricao':'Curso teste 2',
+            'nivel':'A'
+        }
+        response = self.client.post(self.list_url, data=data)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    def test_requisição_delete_para_deletar_curso(self):
+        """Não permitir requisição para deletar curso"""
+        response = self.client.delete('/cursos/1/')
+        self.assertEquals(response.status_code, status. HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_requisicao_put_para_atualizar_curso(self):
+        """Teste para verificar requisicao put, para atualizar um curso"""
+        data = {
+            'codigo_curso':'CT1',
+            'descricao':'Curso teste 1 atualizado',
+            'nivel':'I'
+        }
+        response = self.client.put('/cursos/1/', data=data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
